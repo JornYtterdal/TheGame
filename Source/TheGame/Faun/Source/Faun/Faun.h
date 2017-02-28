ad9216918d2fb2a7c9e239177a2b34c23d94a087
Ollisen <ole_jorgen911@hotmail.com>
2017-02-28 23:02:26 +0100

    Add files via upload

---
+++ Source/TheGame/Faun/Source/Faun/Faun.h
@@ -0,0 +1,11 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __FAUN_H__
+#define __FAUN_H__
+
+#include "EngineMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogFaun, Log, All);
+
+
+#endif
